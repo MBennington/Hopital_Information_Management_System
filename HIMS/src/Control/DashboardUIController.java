fe90923abf4ee210f13a754082564d2391389f6f
Maduka Bennington <w.madhuka@gmail.com>
2020-12-04 23:54:35 +0530

    Update DashboardUIController.java

--- HIMS/src/Control/DashboardUIController.java
+++ HIMS/src/Control/DashboardUIController.java
@@ -2,12 +2,21 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+//update
  */
 package Control;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +24,18 @@ import javafx.fxml.Initializable;
  * @author ASUS
  */
 public class DashboardUIController implements Initializable {
+    
+    @FXML
+    public void Signoutbtn(ActionEvent event) throws IOException{
+        Parent signUpAsParent = FXMLLoader.load(getClass().getResource("/View/UserLogin.fxml"));
+        Scene signUpAsviewScene = new Scene(signUpAsParent);
+        
+        //This Line gets the Stage Information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(signUpAsviewScene);
+        window.show();
+        
+       }
 
     /**
      * Initializes the controller class.
